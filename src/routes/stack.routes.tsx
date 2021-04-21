6aae4cce66857aba33a30025f7c0bbe6407f1210
Luis Felipe Borges Silva <58269629+Lipekline@users.noreply.github.com>
2021-04-21 20:21:50 -0300

    consumindo API fake

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -5,6 +5,7 @@ import fonts from '../styles/fonts'
 import { Welcome } from '../pages/Welcome';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
+import { PlantSelect } from '../pages/PlantSelect';
 
 const stackRoutes = createStackNavigator();
 
@@ -27,6 +28,11 @@ const AppRoutes: React.FC = () => (
         name="Confirmation"
         component = {Confirmation}
         />
+    <stackRoutes.Screen 
+        name="PlantSelect"
+        component = {PlantSelect}
+        />
+
     </stackRoutes.Navigator>
 )
 
